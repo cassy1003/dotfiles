b0b6982394e537454563b11a3c59cebf1348c201
Takahiro KASHIWAGI <takahiro.kashiwagi@enechange.co.jp>
2023-04-24 14:01:25 +0900

    fix zshrc

--- oh-my-zsh/custom/common.plugin.zsh
+++ oh-my-zsh/custom/common.plugin.zsh
@@ -10,20 +10,24 @@ alias vbash="vim ~/.bashrc "
 alias vvim="vim ~/.vimrc "
 
 # git
+alias       g=" git "
 alias     gst=" git status "
 alias     gad=" git add"
 alias    gcom=" git commit"
 alias     gbr=" git branch"
 alias    gcho=" git checkout"
 alias    gpul=" git pull"
-alias    gpus=" git push"
+alias    gpus=" git push origin HEAD"
 alias    grma=" git rm $(git ls-files --deleted)"
-alias    glog=" git log"
-alias glogvim=" git log -p | vim - "
+alias     glg=" git log --stat"
+alias    glgv=" git log -p | vim - "
+alias    glgs=" git log --pretty='format:%C(yellow)%h %C(white)%s %C(red)%d %C(green)[%ad] %C(cyan)<%an>' --date=relative --graph"
+alias    glgm=" git log --no-merges"
 alias     gdf=" git diff "
 alias    gdfc=" git diff --cached"
 alias    gdfl=" git diff | lv"
 alias    gdfv=" git diff | vim - "
+alias    gdfw=" git diff --color-words --word-diff-regex='\\w+|[^[:space:]]'"
 alias    gsub=" git submodule update "
 alias   gsubr=" git submodule update --recursive"
 
@@ -68,6 +72,14 @@ eval "$(rbenv init - zsh)"
 #-------------------------
 #--------- command -------
 #-------------------------
+#
+setopt auto_cd
+setopt nobeep
+
+# 補間
+# autoload -U compinit
+# compinit
+
 # cd ls
 function chpwd() { ls }
 
@@ -75,25 +87,78 @@ function chpwd() { ls }
 function find-grep () { find . -type f -print | xargs grep -n --binary-files=without-match $@ }
 
 # search google
-function google() {
+function ggl() {
     local str opt
         if [ $ != 0 ]; then
             for i in $*; do
                 str="$str+$i"
             done
             str=`echo $str | sed 's/^\+//'`
-            opt='search?num=50&hl=ja&lr=lang_ja'
-            opt="${opt}&q=${str}"
+            opt="?num=30&hl=ja&lr=lang_ja&q=${str}"
         fi
-    w3m http://www.google.co.jp/$opt
+    open http://www.google.co.jp/search$opt
 }
+function gglt() {
+    local str opt
+        if [ $ != 0 ]; then
+            opt="?sl=en&tl=ja&op=translate&hl=ja&text=$*"
+        fi
+    open https://translate.google.com/$opt
+}
+
 
 # search alc
 function alc() {
     if [ $ != 0 ]; then
-        w3m "http://eow.alc.co.jp/$*/UTF-8/?ref=sa"
+        open "http://eow.alc.co.jp/search?q=$*"
     else
-        w3m "http://www.alc.co.jp/"
+        open "http://eow.alc.co.jp/"
     fi
 }
 
+autoload -Uz add-zsh-hook
+add-zsh-hook precmd status_after_checkout
+
+function status_after_checkout() {
+  local prev_cmd="$(fc -ln -1)"
+  if [[ $(echo $prev_cmd | cut -d" " -f 1) = gcho ]]; then
+    if [[ $(echo $prev_cmd | cut -d" " -f 2) != -b ]]; then
+      gst
+    fi
+  fi
+}
+
+## chpwd_tab_color
+tab-color() {
+    echo -ne "\033]6;1;bg;red;brightness;$1\a"
+    echo -ne "\033]6;1;bg;green;brightness;$2\a"
+    echo -ne "\033]6;1;bg;blue;brightness;$3\a"
+}
+
+tab-reset() {
+    echo -ne "\033]6;1;bg;*;default\a"
+}
+
+chpwd_tab_color() {
+  case $PWD/ in
+    */tokyo-gas/*) tab-color 100 0 100;; # purple
+    */emap-*/*) tab-color 100 100 255;; # blue
+    */.ghq/github.com/*) tab-color 100 100 100;; # base gray
+    *) tab-reset;;
+  esac
+}
+add-zsh-hook chpwd chpwd_tab_color
+
+function virtualenv_info {
+    [ $VIRTUAL_ENV ] && echo '('`basename $VIRTUAL_ENV`') '
+}
+
+function box_name {
+    [ -f ~/.box-name ] && cat ~/.box-name || hostname -s
+}
+local current_dir='${PWD/#$HOME/~}'
+local git_info='$(git_prompt_info)'
+
+PROMPT="%{$FG[239]%}────────────────────%{$reset_color%}
+%{$FG[040]%}- %{$reset_color%}%{$FG[239]%}in%{$reset_color%} %{$terminfo[bold]$FG[226]%}${current_dir}%{$FG[033]%}${git_info} %{$FG[239]%}: %n at $(box_name)%{$reset_color%} $(virtualenv_info)
+%{$FG[040]%}>>%{$reset_color%}%} "
